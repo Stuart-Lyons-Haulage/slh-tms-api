70452a56bb6add96f99d86b6757529d863c4dded
Daniel Williams <danielwilliams@Mac.mynet>
2026-08-11 23:55:06 +0100

    Fix integration test service imports

--- Slh.Tms.Api.Tests/CustomWebFactory.cs
+++ Slh.Tms.Api.Tests/CustomWebFactory.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Slh.Tms.Api.Data;
 
 namespace Slh.Tms.Api.Tests;
