95cb660f7b96ce980a4e822f30b6c4f90c207842
danwilliams201302-max <daniel@lyonshaulage.com>
2026-08-24 22:05:37 +0100

    Include site address in geofence matching

--- Services/GeofenceSiteResolver.cs
+++ Services/GeofenceSiteResolver.cs
@@ -21,6 +21,7 @@ public static class GeofenceSiteResolver
                 ExternalCode = x.ExternalCode,
                 Name = x.Name,
                 DriverTextName = x.DriverTextName,
+                CollectionAddress = x.CollectionAddress,
                 Active = x.Active
             })
             .ToListAsync(ct);
