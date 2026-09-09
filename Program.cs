4b62fd358075edb864fc97d0a3991743554abe1e
danwilliams201302-max <daniel@lyonshaulage.com>
2026-09-09 20:47:34 +0100

    Share TachoMaster responses across all API consumers

--- Program.cs
+++ Program.cs
@@ -165,6 +165,7 @@ builder.Services.AddScoped<DriverMasterClassificationService>();
 builder.Services.AddScoped<TachoCanonicalDriverMasterOrchestrator>();
 builder.Services.AddScoped<TachoDriverMasterSyncJobService>();
 builder.Services.AddTransient<TachoMasterRetryHandler>();
+builder.Services.AddTransient<TachoMasterResponseCacheHandler>();
 builder.Services.AddTransient<DependencyTelemetryHandler>();
 builder.Services.AddTransient<ProviderResilienceHandler>();
 builder.Services.AddScoped<DriverWeeklyRestComplianceService>();
@@ -173,6 +174,7 @@ builder.Services.AddHttpClient<SageHrClient>().AddHttpMessageHandler<DependencyT
 builder.Services.AddHttpClient<DotTrackingClient>().AddHttpMessageHandler<DependencyTelemetryHandler>().AddHttpMessageHandler<ProviderResilienceHandler>();
 builder.Services.AddHttpClient<TachoMasterClient>()
     .AddHttpMessageHandler<DependencyTelemetryHandler>()
+    .AddHttpMessageHandler<TachoMasterResponseCacheHandler>()
     .AddHttpMessageHandler<TachoMasterRetryHandler>()
     .ConfigureHttpClient((sp, _) =>
     {
