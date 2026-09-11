2bd7cae61a4bd31ffd5008fc60554faa0552f035
danwilliams201302-max <daniel@lyonshaulage.com>
2026-09-11 14:29:44 +0100

    Repair startup migration deployment wiring (#362)

--- Database/043_Customer_Site_Crm_Links.sql
+++ Database/043_Customer_Site_Crm_Links.sql
@@ -25,8 +25,6 @@ BEGIN
     );
 END;
 
-IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'IX_Customers_Code' AND object_id = OBJECT_ID(N'dbo.Customers'))
-    CREATE UNIQUE INDEX IX_Customers_Code ON dbo.Customers(Code);
 IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'IX_Sites_CustomerCode_ExternalCode' AND object_id = OBJECT_ID(N'dbo.Sites'))
     CREATE INDEX IX_Sites_CustomerCode_ExternalCode ON dbo.Sites(CustomerCode, ExternalCode);
 IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'IX_CustomerEmailRoutes_CustomerCode_SenderEmail_SenderDomain' AND object_id = OBJECT_ID(N'dbo.CustomerEmailRoutes'))
