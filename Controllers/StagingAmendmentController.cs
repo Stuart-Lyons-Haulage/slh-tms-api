e9478e695eab840b2b8cb7cc3b999bea3551cf14
danwilliams201302-max <daniel@lyonshaulage.com>
2026-09-15 11:02:28 +0100

    Persist confirmed delivery site aliases to SQL

--- Controllers/StagingAmendmentController.cs
+++ Controllers/StagingAmendmentController.cs
@@ -90,6 +90,9 @@ public sealed class StagingAmendmentController(TmsDbContext db) : ControllerBase
 
             if (changed)
             {
+                db.Sites.Attach(selected);
+                db.Entry(selected).Property(site => site.Aliases).IsModified = true;
+
                 db.MasterDataAudits.Add(new MasterDataAudit
                 {
                     EntityType = "Site",
