74e1078cd2cd8014a87a66b06d843b91864505f3
danwilliams201302-max <daniel@lyonshaulage.com>
2026-09-16 10:24:15 +0100

    Remove broad nullable Guid extension

--- Services/GuidCollectionExtensions.cs
+++
@@ -1,7 +0,0 @@
-namespace Slh.Tms.Api.Services;
-
-internal static class GuidCollectionExtensions
-{
-    public static bool Contains(this IReadOnlyCollection<Guid> values, Guid? value) =>
-        value.HasValue && values.Contains(value.Value);
-}
