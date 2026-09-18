ad47eb64e4794b23fad9ed4d1ad75e841118de07
danwilliams201302-max <daniel@lyonshaulage.com>
2026-09-18 12:40:17 +0100

    Make Summer Berry workbook fixture Excel-compatible

--- Slh.Tms.Api.Tests/SummerBerryWorkbookParserTests.cs
+++ Slh.Tms.Api.Tests/SummerBerryWorkbookParserTests.cs
@@ -56,6 +56,8 @@ public sealed class SummerBerryWorkbookParserTests
                   <Default Extension="xml" ContentType="application/xml"/>
                   <Override PartName="/xl/workbook.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml"/>
                   <Override PartName="/xl/worksheets/sheet1.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"/>
+                  <Override PartName="/xl/sharedStrings.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sharedStrings+xml"/>
+                  <Override PartName="/xl/styles.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.styles+xml"/>
                 </Types>
                 """);
             Add(archive, "_rels/.rels", """
@@ -74,31 +76,46 @@ public sealed class SummerBerryWorkbookParserTests
                 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
                 <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
                   <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="worksheets/sheet1.xml"/>
+                  <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings" Target="sharedStrings.xml"/>
+                  <Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles" Target="styles.xml"/>
                 </Relationships>
+                """);
+            Add(archive, "xl/sharedStrings.xml", """
+                <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+                <sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" count="11" uniqueCount="11">
+                  <si><t>Collection Site</t></si><si><t>Date</t></si><si><t>Depot Description</t></si>
+                  <si><t>Pallets</t></si><si><t>Request time</t></si><si><t>Available time</t></si>
+                  <si><t>SB-Groves Farm</t></si><si><t>MORRISONS SITTINGBOURNE</t></si>
+                  <si><t>SB-Groves Farm</t></si><si><t>ALDI GOLDTHORPE</t></si><si><t>unused</t></si>
+                </sst>
+                """);
+            Add(archive, "xl/styles.xml", """
+                <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+                <styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
+                  <fonts count="1"><font/></fonts><fills count="1"><fill><patternFill patternType="none"/></fill></fills>
+                  <borders count="1"><border/></borders><cellStyleXfs count="1"><xf/></cellStyleXfs>
+                  <cellXfs count="1"><xf xfId="0"/></cellXfs>
+                </styleSheet>
                 """);
             Add(archive, "xl/worksheets/sheet1.xml", """
                 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
                 <worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
                   <sheetData>
                     <row r="1">
-                      <c r="A1" t="inlineStr"><is><t>Collection Site</t></is></c>
-                      <c r="B1" t="inlineStr"><is><t>Date</t></is></c>
-                      <c r="C1" t="inlineStr"><is><t>Depot Description</t></is></c>
-                      <c r="D1" t="inlineStr"><is><t>Pallets</t></is></c>
-                      <c r="E1" t="inlineStr"><is><t>Request time</t></is></c>
-                      <c r="F1" t="inlineStr"><is><t>Available time</t></is></c>
+                      <c r="A1" t="s"><v>0</v></c><c r="B1" t="s"><v>1</v></c><c r="C1" t="s"><v>2</v></c>
+                      <c r="D1" t="s"><v>3</v></c><c r="E1" t="s"><v>4</v></c><c r="F1" t="s"><v>5</v></c>
                     </row>
                     <row r="2">
-                      <c r="A2" t="inlineStr"><is><t>SB-Groves Farm</t></is></c>
+                      <c r="A2" t="s"><v>6</v></c>
                       <c r="B2"><v>46284</v></c>
-                      <c r="C2" t="inlineStr"><is><t>MORRISONS SITTINGBOURNE</t></is></c>
+                      <c r="C2" t="s"><v>7</v></c>
                       <c r="D2"><v>10</v></c>
                       <c r="E2"><v>0.2916666667</v></c>
                     </row>
                     <row r="3">
-                      <c r="A3" t="inlineStr"><is><t>SB-Groves Farm</t></is></c>
+                      <c r="A3" t="s"><v>8</v></c>
                       <c r="B3"><v>46284</v></c>
-                      <c r="C3" t="inlineStr"><is><t>ALDI GOLDTHORPE</t></is></c>
+                      <c r="C3" t="s"><v>9</v></c>
                     </row>
                   </sheetData>
                 </worksheet>
